50137a4072f9067d52c0e990e910b9ed830a79cb
AndyCatlow <mrcatlow@gmail.com>
2021-06-25 08:27:19 +0200

    remove font awesome request from success.html

---
+++ Site/success.js
@@ -0,0 +1,37 @@
+/* <div class="success__container hidden">
+      <section class="success">
+        <h1 class="success__thank">Thank you</h1>
+        <p class="success__text">
+          Your enquiry has been received and I'll get back to you as soon as
+          possible.
+        </p>
+        <button class="button return">
+          Return to Gallery
+        </button> */
+
+// const successContainer = document.getElementsByClassName("success__container");
+// const successThank = document.getElementsByClassName("success__thank");
+// const successText = document.getElementsByClassName("success__text");
+// const form = document.getElementsByName("enquiry");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   let formData = new FormData(event.target);
+//   let firstName = formData.get("name");
+//   let userEmail = formData.get("email");
+//   let painting = formData.get("paintingName");
+//   let updatedSuccess = `
+
+//       <section class="success">
+//         <h1 class="success__thank">Thank you ${firstName}</h1>
+//         <p class="success__text">
+//           Your enquiry about ${painting} has been received and I'll get back to you at ${userEmail} as soon as
+//           possible.
+//         </p>
+//         <button class="button return">Return to Gallery</button>
+//       </section>
+
+//   `;
+
+//   successContainer.innerHTML = updatedSuccess;
+// });
